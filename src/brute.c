8fefccc12c8d5cd3a2f185f7ed0c6ea3c329ebe6
Murilo Schirmer <muriloschirmer16@gmail.com>
2018-11-17 12:23:09 -0200

    ta ai

---
+++ src/brute.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "key.h"
+#include "brute.h"
+
+struct sums {
+  int lines[N];
+  int lastline;
+  Key sum;
+};
+
+// INICIALIZA UMA POSIÇÃO DO VETOR DE SOMAS.
+Sums VALUE (Key T[N], int i) {
+  Sums x;
+  x.sum = T[i];
+  x.lines[0] = i;
+  x.lastline = 0;
+  return x;
+}
+
+// FAZ A SOMA DE X COM O VALOR NA TABELA T NO ÍNDICE I, ALÉM DE ACRESCER A LASTLINE E TAMBÉM INSERE A LINHA
+Sums SUM (Sums X, Key T[N], int i) {
+  X.sum = add (X.sum , T[i]);
+  X.lastline++;
+  X.lines[X.lastline] = i;
+  return X;
+}
+
+// RETORNA A PRIMEIRA LINHA
+int FIRSTLINE (Sums sum) {
+  return sum.lines[0];
+}
+
+// RETORNA A ÚLTIMA LINHA
+int LASTLINE (Sums sum) {
+  return sum.lines[sum.lastline];
+}
+
+// FAZ TODAS AS POSSÍVEIS COMBINAÇÕES DE SOMAS E AS ARMAZENA EM 'sumsVector'
+Sums* SUM_BRUTE_FORCE (Key T[N]) {
+  //Sums* sumsVector = malloc (MAXPOS * sizeof(Sums*));
+  Sums* sumsVector = malloc (MAXPOS * sizeof * sumsVector);
+
+  sumsVector[0] = VALUE (T, 0);
+  //print_key_char (sumsVector[0].sum);
+  int k = 0;
+  int pos = 1;
+  int i = 1;
+  printf("%d\n", N);
+  printf("%d\n", MAXPOS);
+  while (pos != MAXPOS) {
+    printf("%d\n", pos);
+    sumsVector[pos] = SUM (sumsVector[k], T, i);
+    //print_key_char (sumsVector[pos].sum);
+    pos++;
+    i++;
+    if (i == MAXLINE) {
+      k++;
+      i = LASTLINE (sumsVector[k]);
+      if (i == MAXLINE) {
+        sumsVector[pos] = VALUE (T, FIRSTLINE (sumsVector[pos-1]));
+        k = pos;
+        pos++;
+        i  = LASTLINE (sumsVector[k]);
+      }
+    }
+  }
+
+  return sumsVector;
+}
+
+// COMPARA DUAS KEYS, RETORNANDO TRUE SE IGUAIS OU FALSE SE DIFERENTES
+bool COMPARE (Key a, Sums b) {
+  //print_key_char (a); printf(" - "); print_key_char(b.sum);printf("\n");
+  for (int i = 0; i < C; i++) {
+    if(a.digit[i] != b.sum.digit[i]) {
+      return FALSE;
+    }
+  }
+  return TRUE;
+}
+
+// IMPRIME A POSSÍVEL SENHA
+void PRINT (Sums a) {
+  Key possible = {{0}};
+  for (int i = 0; i < a.lastline; i++) {
+    possible.digit[a.lines[i]] = 1;
+  }
+  print_key(possible);
+
+  print_key_char (possible);
+}
+
+// FAZ A COMPARAÇÃO DE TODAS AS POSSÍVEIS SENHAS E AS IMPRIME
+void COMPARE_AND_PRINT (Key crypt, Sums* sumsVector) {
+  for (int i = 0; i < MAXPOS; i++) {
+    if(COMPARE(crypt, sumsVector[i])){
+      PRINT(sumsVector[i]);
+    }
+  }
+}
+
+int main (int argc, char *argv[]) {
+  Key cript = init_key((unsigned char *) argv[1]);
+  Key T[N];
+  //reaproveitamento de código
+  unsigned char buffer[C+1];
+  for (int i = 0; i < N; i++) {
+    scanf ("%s", buffer);
+    T[i] = init_key(buffer);
+  }
+  Sums* sumsVector = SUM_BRUTE_FORCE(T);
+  COMPARE_AND_PRINT (cript, sumsVector);
+
+  free (sumsVector);
+
+  return 0;
+}
---
+++ src/brute.h
@@ -0,0 +1,40 @@
+#ifndef BRUTE_H_
+#define BRUTE_H_
+
+#include "key.h"
+#include <math.h>
+// TYPEDEF PARA STRUCT SUMS
+typedef struct sums Sums;
+
+#define FALSE false
+#define TRUE true
+// NÚMERO MÁXIMO DE POSIÇÕES
+#define MAXPOS (int)((pow(2, N))-1)
+// NÚMERO MÁXIMO DE LINHAS
+#define MAXLINE N
+
+// INICIALIZA UMA POSIÇÃO DO VETOR DE SOMAS.
+Sums VALUE (Key T[N], int i);
+
+// FAZ A SOMA DE X COM O VALOR NA TABELA T NO ÍNDICE I, ALÉM DE ACRESCER A LASTLINE E TAMBÉM INSERE A LINHA
+Sums SUM (Sums X, Key T[N], int i);
+
+// RETORNA A PRIMEIRA LINHA
+int FIRSTLINE (Sums sum);
+
+// RETORNA A ÚLTIMA LINHA
+int LASTLINE (Sums sum);
+
+// FAZ TODAS AS POSSÍVEIS COMBINAÇÕES DE SOMAS E AS ARMAZENA EM 'sumsVector'
+Sums* SUM_BRUTE_FORCE (Key T[N]);
+
+// COMPARA DUAS KEYS, RETORNANDO TRUE SE IGUAIS OU FALSE SE DIFERENTES
+bool COMPARE (Key a, Sums b);
+
+// IMPRIME A POSSÍVEL SENHA
+void PRINT (Sums a);
+
+// FAZ A COMPARAÇÃO DE TODAS AS POSSÍVEIS SENHAS E AS IMPRIME
+void COMPARE_AND_PRINT (Key crypt, Sums* sumsVector);
+
+#endif
